d7c45f8a715a9f41a9892470781530e70c3c2bc5
Muhammad Junaid <muhammadjunaidab@example.com>
2025-09-03 22:40:52 -0700

    Trigger GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/basic-todo-app/', // ✅ use repo name here, not GitHub link
+  base: '/basic-todo-app/',
 })
